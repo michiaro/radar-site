01796963c41a814b7bd4d42e7df102e57d0e60c8
dubaua <dubaua@gmail.com>
2019-05-17 15:55:48 +0500

    drastically simplify navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,44 +1,89 @@
 <template lang="pug">
-  nav.main-menu(:class="{'mobile' : isMobile}")
-    ul.main-menu__list
-      li.main-menu__item
-        router-link.main-menu__link(
-          to="/all-works",
-        )
-          | РАБОТЫ
-      li.main-menu__item
-        router-link.main-menu__link(
-          to="/about",
-        )
-          | О НАС
-      li.main-menu__item
-        router-link.main-menu__link(
-          to="/team",
-        )
-          | КОМАНДА
-      li.main-menu__item
-        router-link.main-menu__link(
-          to="/clients",
-        )
-          | КЛИЕНТЫ
-      li.main-menu__item
-        router-link.main-menu__link(
-          to="/contacts",
-        )
-          | КОНТАКТЫ
+  nav.navigation
+    router-link.navigation__link(v-for="link in menu" :to="link.to") {{ link.label }}
 </template>
 
 <script>
 export default {
-  name: 'Navigation',
+  name: "Navigation",
   props: {
     isMobile: {
       type: Boolean,
     },
   },
+  data() {
+    return {
+      menu: [
+        {
+          label: "РАБОТЫ",
+          to: "/all-works"
+        },
+        {
+          label: "О НАС",
+          to: "/about"
+        },
+        {
+          label: "КОМАНДА",
+          to: "/team"
+        },
+        {
+          label: "КЛИЕНТЫ",
+          to: "/clients"
+        },
+        {
+          label: "КОНТАКТЫ",
+          to: "/contacts"
+        },
+      ],
+    };
+  },
 }
 </script>
 
 <style lang="scss">
+@import '~@/styles/shared/globals';
+
+.navigation {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 999;
+  padding: 66px 0;
+  background: #e30613;
+
+  @include from-breakpoint('xl') {
+    position: static;
+    z-index: 0;
+    padding: 0;
+    display: flex;
+    background: none;
+  }
+
+  &__link {
+    display: block;
+    color: #fff;
+    border-bottom: 1px solid white;
+    font-size: 18px;
+    padding: 1em;
+
+    &:hover {
+      opacity: 0.8;
+      @include from-breakpoint('xl') {
+        color: #e10811;
+      }
+    }
+
+    @include from-breakpoint('xl') {
+      font-size: 14px;
+      border-bottom: 0;
+
+      .router-link-active {
+        color: #e10811;
+      }
+    }
+  }
+}
 
 </style>
