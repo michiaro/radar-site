0e8fe74e0b6b2565a7efa56b061544e82ee89813
Julia Pospelova <michiaro@bk.ru>
2020-08-03 03:45:01 +0500

    add error page, new audio work component

--- src/components/Pages/ErrorPage.vue
+++ src/components/Pages/ErrorPage.vue
@@ -1,13 +1,107 @@
 <template>
-  <h1>Страница ошибки</h1>
+  <div class="error-page">
+    <div class="error-page__inner">
+      <appear :is-visible="animationCounter >= 0" :on-next="showNext">
+        <div class="appear appear--up">
+          <img :src="image" alt="404" class="error-page__image" />
+        </div>
+      </appear>
+      <appear :is-visible="animationCounter >= 1" :on-next="showNext">
+        <div class="appear appear--up">
+          <h1 class="error-page__title">
+            Похоже, такой страницы не существует.
+          </h1>
+        </div>
+      </appear>
+      <appear :is-visible="animationCounter >= 2" :on-next="showNext">
+        <div class="appear appear--up">
+          <router-link class="error-page__link" :to="{ name: 'Home' }">
+            Вернуться на главную?
+          </router-link>
+        </div>
+      </appear>
+    </div>
+  </div>
 </template>
 
 <script>
+import Appear from '@/components/Appear.vue';
+import image from '@/images/404.png';
+
 export default {
   name: 'ErrorPage',
+  components: {
+    Appear,
+  },
+  data() {
+    return {
+      image,
+      animationCounter: -1,
+    };
+  },
+  mounted() {
+    this.showNext();
+  },
+  methods: {
+    showNext() {
+      this.animationCounter++;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
+@import '@/styles/shared/_globals.scss';
 
+.error-page {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - #{$--header-height});
+
+  &__inner {
+    margin-bottom: $--header-height;
+  }
+  &__title {
+    font-weight: normal;
+    font-size: $--font-size-140;
+    margin: 2em 0 0.2em;
+  }
+  &__link {
+    font-weight: 500;
+    font-size: $--font-size-140;
+    color: $--color-brand;
+    display: inline-block;
+    position: relative;
+    margin-left: $--page-padding-x;
+    @include from('lg') {
+      margin-left: 0;
+    }
+
+    &:after {
+      content: '';
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 100%;
+      height: 1px;
+      background: $--color-brand;
+      transition-timing-function: $--timing-in-out-cubic;
+      transition-duration: $--duration-1000;
+      transition-property: transform;
+      transform-origin: left center;
+      transform: scaleX(0);
+    }
+
+    @at-root ._ready &:after {
+      transform: scaleX(1);
+      transition-delay: $--delay-1000;
+    }
+
+    &:hover:after {
+      transform: scaleX(0);
+    }
+  }
+}
 </style>
---
+++ src/components/work-components/Audiofile.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="work-audio">
+    <iframe
+      width="100%"
+      height="166"
+      scrolling="no"
+      frameborder="no"
+      allow="autoplay"
+      :src="
+        'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' +
+          settings.audio +
+          '&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true'
+      "
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Audiofile',
+  props: {
+    settings: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.work-audio {
+  margin-bottom: 1vmax;
+}
+</style>
