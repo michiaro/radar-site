9de6d183f5187c177cd66f77ece7392c190b6793
Julia Pospelova <julia.pospelova@fuse8.ru>
2020-12-16 03:24:57 +0500

    add link with arrow to services

--- src/components/services/ServicePageDirection.vue
+++ src/components/services/ServicePageDirection.vue
@@ -8,7 +8,11 @@
     @click="passReference"
   >
     <video
-      ref="video" class="service__video" muted="muted" playsinline preload="auto"
+      ref="video"
+      class="service__video"
+      muted="muted"
+      playsinline
+      preload="auto"
       :loop="isLooped"
     >
       <source :src="videoURL + video.path" type="video/mp4" />
@@ -18,6 +22,28 @@
       <p class="service__description">
         {{ glueUpPrepositions(description) }}
       </p>
+
+      <div class="service__bottom">
+        <p class="service__link">детали и примеры</p>
+
+        <svg
+          class="service__arrow"
+          viewBox="0 0 51 45"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M46.9609 23.7041H0V20.7041H46.9609V23.7041Z"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M31.272 0.020813L50.3824 22.1973L31.2788 44.6787L28.9927 42.7361L46.434 22.2109L28.9994 1.97921L31.272 0.020813Z"
+          />
+        </svg>
+      </div>
     </div>
   </div>
 </template>
@@ -86,7 +112,10 @@ export default {
 
   // вписываем все три плашки в один экран по высоте
   // вычитаем высоту шапки, отступы между плашками и от шапки
-  height: calc((100vh - #{$--header-height} - (#{$--gutter} * 3) - #{$--page-padding-x}) / 3);
+  height: calc(
+    (100vh - #{$--header-height} - (#{$--gutter} * 3) - #{$--page-padding-x}) /
+      3
+  );
   margin-bottom: $--gutter;
   overflow: hidden;
 
@@ -104,6 +133,10 @@ export default {
     bottom: 0;
     left: 0;
     padding: $--gutter;
+
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-between;
   }
 
   &__title {
@@ -122,6 +155,7 @@ export default {
   }
 
   &__description {
+    flex-grow: 1;
     margin: 0;
     margin-top: 1.5vmax;
     font-size: $--font-size-100;
@@ -134,7 +168,8 @@ export default {
       opacity: 0;
       transform: translate(0, 3vmax);
       transition-duration: $--duration-2000, $--duration-2000;
-      transition-timing-function: $--timing-in-out-spring, $--timing-in-out-spring;
+      transition-timing-function: $--timing-in-out-spring,
+        $--timing-in-out-spring;
       transition-property: opacity, transform;
     }
   }
@@ -145,11 +180,69 @@ export default {
     height: auto;
   }
 
+  &__bottom {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    @include from('xl') {
+      opacity: 0;
+      transform: translate(0, 3vmax);
+      transition-delay: $--delay-0;
+      transition-duration: $--duration-2000, $--duration-2000;
+      transition-timing-function: $--timing-in-out-spring,
+        $--timing-in-out-spring;
+      transition-property: opacity, transform;
+    }
+  }
+
+  &__link {
+    font-weight: 300;
+    font-size: $--font-size-90;
+    color: $--color-brand;
+    display: inline-block;
+    position: relative;
+    margin-left: $--page-padding-x;
+    @include from('lg') {
+      margin-left: 0;
+    }
+
+    &:after {
+      content: '';
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 100%;
+      height: 1px;
+      background: $--color-brand;
+      transition-timing-function: $--timing-in-out-cubic;
+      transition-duration: $--duration-1000;
+      transition-property: transform;
+      transform-origin: left center;
+      transition-delay: $--delay-1000;
+      transform: scaleX(0);
+    }
+  }
+
+  &__arrow {
+    width: 51px;
+    height: 45px;
+    color: $--color-text;
+  }
+
   &:hover {
-    #{$service}__description {
-      @include from('xl') {
+    @include from('xl') {
+      #{$service}__description {
         opacity: 1;
         transform: translate(0, 0);
+      }
+      #{$service}__bottom {
+        opacity: 1;
+        transform: translate(0, 0);
+        transition-delay: $--duration-800;
+      }
+      #{$service}__link:after {
+        transform: scaleX(1);
       }
     }
   }
