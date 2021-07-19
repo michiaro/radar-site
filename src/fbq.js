a02951bbbd372c3941e06fc9a41941e775a3604c
woobla <woobla@yandex.ru>
2021-07-19 16:26:50 +0500

    правка под eslint

--- src/fbq.js
+++ src/fbq.js
@@ -1,53 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
-    if ('fbq' in window) {
-        targetFBQActions();
-    }
+  if ('fbq' in window) {
+    targetFBQActions();
+  }
 });
+
 function targetFBQActions() {
-    document.addEventListener('services-campaign', function () {
-        //console.log('services-campaign');
-        fbq('track', 'ViewContent', {
-            content_type: 'campaign',
-        });
+  document.addEventListener('services-campaign', () => {
+    //console.log('services-campaign');
+    // eslint-disable-next-line no-undef
+    fbq('track', 'ViewContent', {
+      content_type: 'campaign',
     });
-    document.addEventListener('services-branding', function () {
-        //console.log('services-branding');
-        fbq('track', 'ViewContent', {
-            content_type: 'branding',
-        });
+  });
+  document.addEventListener('services-branding', () => {
+    //console.log('services-branding');
+    // eslint-disable-next-line no-undef
+    fbq('track', 'ViewContent', {
+      content_type: 'branding',
     });
-    document.addEventListener('services-digital', function () {
-        //console.log('services-digital');
-        fbq('track', 'ViewContent', {
-            content_type: 'digital',
-        });
+  });
+  document.addEventListener('services-digital', () => {
+    //console.log('services-digital');
+    // eslint-disable-next-line no-undef
+    fbq('track', 'ViewContent', {
+      content_type: 'digital',
     });
-    document.addEventListener('pillars-strategy', function () {
-        //console.log('pillars-digital');
-        fbq('track', 'ViewContent', {
-            content_type: 'strategy',
-        });
+  });
+  document.addEventListener('pillars-strategy', () => {
+    //console.log('pillars-digital');
+    // eslint-disable-next-line no-undef
+    fbq('track', 'ViewContent', {
+      content_type: 'strategy',
     });
-    document.addEventListener('pillars-creative', function () {
-        //console.log('pillars-creative');
-        fbq('track', 'ViewContent', {
-            content_type: 'creative',
-        });
+  });
+  document.addEventListener('pillars-creative', () => {
+    //console.log('pillars-creative');
+    // eslint-disable-next-line no-undef
+    fbq('track', 'ViewContent', {
+      content_type: 'creative',
     });
-    document.addEventListener('pillars-design', function () {
-        //console.log('pillars-design');
-        fbq('track', 'ViewContent', {
-            content_type: 'design',
-        });
+  });
+  document.addEventListener('pillars-design', () => {
+    //console.log('pillars-design');
+    // eslint-disable-next-line no-undef
+    fbq('track', 'ViewContent', {
+      content_type: 'design',
     });
-    document.addEventListener('pillars-innovation', function () {
-        //console.log('pillars-innovation');
-        fbq('track', 'ViewContent', {
-            content_type: 'innovation',
-        });
+  });
+  document.addEventListener('pillars-innovation', () => {
+    //console.log('pillars-innovation');
+    // eslint-disable-next-line no-undef
+    fbq('track', 'ViewContent', {
+      content_type: 'innovation',
     });
-    document.addEventListener('form-submit-success', function () {
-        //console.log('form-submit-success');
-        fbq('track', 'Lead');
-    });
+  });
+  document.addEventListener('form-submit-success', () => {
+    //console.log('form-submit-success');
+    // eslint-disable-next-line no-undef
+    fbq('track', 'Lead');
+  });
 }
